82c50eb216413b1537f8f456fe321932ed79564c
Andres Verdenelli <andresverdenelli@icloud.com>
2024-12-29 11:10:47 -0300

    fix: update build and asset configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
   },
-  base: '/',
+  base: './',
 })
